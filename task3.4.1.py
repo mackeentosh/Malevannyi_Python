5624314efdfa1a6d47c70b724874f58b8e49ae3d
Артём Малеванный <artem.irk16@gmail.com>
2022-12-30 21:09:40 +0500

    3.4.1 commit

---
+++ task3.4.1.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import math
+
+
+def convert_currency(string, df_currency):
+    if pd.isnull(string):
+        return string
+    arr = string.split()
+    if df_currency.columns.__contains__(arr[1]):
+        date = arr[2]
+        course = df_currency[df_currency["date"] == date[:7]][arr[1]].values
+        if not math.isnan(course[0]):
+            return round(float(arr[0]) * course[0])
+    return arr[0]
+
+
+def convert_currencies_in_file():
+    file_name = "vacancies_dif_currencies.csv"
+    file_currencies = "currencies_years.csv"
+    dataframe = pd.read_csv(file_name)
+    dataframe_curr = pd.read_csv(file_currencies)
+    dataframe.insert(1, "salary", None)
+    dataframe.assign(salary=lambda x: x.salary_from)
+    dataframe["salary"] = dataframe[["salary_from", "salary_to"]].mean(axis=1)
+    dataframe["salary"] = dataframe["salary"].astype(str) + " " + + dataframe["salary_currency"] + " " + dataframe["published_at"]
+    dataframe["salary"] = dataframe["salary"].apply(lambda x: convert_currency(x, dataframe_curr))
+    dataframe = dataframe.drop(columns=['salary_from', 'salary_to', 'salary_currency'])
+    result = dataframe.head(100)
+    result.to_csv("converted_dataframe.csv", index=False)
+
+
+convert_currencies_in_file()
